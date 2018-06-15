797c331415526f70d2e126375da7391c662725d8
Benjamin Perkins <live@perkinsville.com>
2018-06-15 09:58:46 +0200

    Adding some code for setting new articles

---
+++ csharpguitar/LocalizeASP/LocalizeASP.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="LocalizeASP.aspx.cs" Inherits="_Default" culture="auto" meta:resourcekey="PageResource1" uiculture="auto" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <br /><br /><br />
+        <asp:Table ID="Table1" runat="server" >
+            <asp:TableRow >
+                <asp:TableCell >
+                    <asp:Label ID="Label1" runat="server" Text="First Name:" meta:resourcekey="Label1Resource1" />
+                </asp:TableCell><asp:TableCell>
+                    <asp:TextBox ID="textBox1" runat="server"/>
+                </asp:TableCell></asp:TableRow><asp:TableRow>
+                <asp:TableCell>
+                    <asp:Label ID="Label2" runat="server" Text="Last Name:" meta:resourcekey="Label2Resource1" />
+                </asp:TableCell><asp:TableCell>
+                    <asp:TextBox ID="textBox2" runat="server"/>
+                </asp:TableCell></asp:TableRow><asp:TableRow>
+                <asp:TableCell>
+                    <asp:Label ID="Label3" runat="server" Text="Address:" meta:resourcekey="Label3Resource1" />
+                </asp:TableCell><asp:TableCell>
+                    <asp:TextBox ID="textBox3" runat="server"/>
+                </asp:TableCell></asp:TableRow><asp:TableRow HorizontalAlign="Right">
+                <asp:TableCell ColumnSpan="2">
+                    <asp:Button ID="Button1" runat="server" Text="Submit" meta:resourcekey="Button1Resource1" />
+                </asp:TableCell></asp:TableRow></asp:Table></div></form></body></html>
---
+++ csharpguitar/ReadConfig/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Configuration;
+using System.Collections.Specialized;
+using static System.Console;
+
+namespace ConfigElementAndSection
+{
+    class Program
+    {
+        public class ConfigurationMgr
+        {
+            public void ReadAllAppSettings()
+            {
+                try
+                {
+                    NameValueCollection appSettings = ConfigurationManager.AppSettings;
+
+                    if (appSettings.Count == 0)
+                    {
+                        WriteLine("ReadAppSettings: The AppSettings section is empty.");
+                        WriteLine();
+                    }
+
+                    for (int i = 0; i < appSettings.Count; i++)
+                    {
+                        WriteLine($"ReadAppSettings: {i} Key: {appSettings.GetKey(i)} Value: {appSettings[i]}");
+                        WriteLine();
+                    }
+                }
+                catch (ConfigurationErrorsException e)
+                {
+                    WriteLine($"ReadAppSettings: {e.ToString()}");
+                    WriteLine();
+                }
+            }
+
+            public void CreateAppSettings(string sectionName, string key, string value)
+            {
+                try
+                {
+                    Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+                    config.AppSettings.Settings.Add(key, value);
+
+                    config.Save();
+                    ConfigurationManager.RefreshSection(sectionName);
+                }
+                catch (ConfigurationErrorsException e)
+                {
+                    WriteLine($"[CreateAppSettings: {e.ToString()}]");
+                    WriteLine();
+                }
+            }
+
+            public string GetAppSettingValue(string key)
+            {
+                try
+                {
+                    NameValueCollection appSettings = ConfigurationManager.AppSettings;
+
+                    string[] arr = appSettings.GetValues(key);
+                    return arr[0];
+                }
+                catch (ConfigurationErrorsException e)
+                {
+                    Console.WriteLine($"[CreateAppSettings: {e.ToString()}]");
+                    Console.WriteLine();
+                    return e.ToString();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"[CreateAppSettings: {e.ToString()}]");
+                    Console.WriteLine();
+                    return e.ToString();
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ConfigurationMgr configMgr = new ConfigurationMgr();
+
+            Console.WriteLine("***** appSettings before adding section, element and element items *****");
+            Console.WriteLine();
+            configMgr.ReadAllAppSettings();
+
+            configMgr.CreateAppSettings("appSettings", "KEY0", "VALUE0");
+
+            configMgr.CreateAppSettings("appSettings", "KEY1", "VALUE1");
+
+            configMgr.CreateAppSettings("appSettings", "KEY2", "VALUE2");
+
+            Console.WriteLine("***** appSettings after adding section, element and element items *****");
+            Console.WriteLine();
+            configMgr.ReadAllAppSettings();
+
+            Console.WriteLine("appSettings value for KEY0 is {0}", configMgr.GetAppSettingValue("KEY0"));
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
