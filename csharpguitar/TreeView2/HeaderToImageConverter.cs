ee55ba543ffe0d0c80a8f876d96aee79cee0854d
Benjamin Perkins <benperk@microsoft.com>
2020-05-26 12:19:04 +0200

    Add files via upload

---
+++ csharpguitar/TreeView2/HeaderToImageConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace TreeView
+{
+    [ValueConversion(typeof(string), typeof(bool))]
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TreeViewModel)
+            {
+                TreeViewModel tvm = (TreeViewModel)value;
+
+                if (tvm.IsRoot)
+                {
+                    Uri uri = new Uri("pack://application:,,,/green.png");
+                    BitmapImage source = new BitmapImage(uri);
+                    return source;
+                }
+                else
+                {
+                    Uri uri = new Uri("pack://application:,,,/blue.png");
+                    BitmapImage source = new BitmapImage(uri);
+                    return source;
+                }
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("Cannot convert back");
+        }
+    }
+}
---
+++ csharpguitar/TreeView2/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TreeView
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            treeView1.ItemsSource = TreeViewModel.SetTree("Top Level");
+        }
+    }
+}
