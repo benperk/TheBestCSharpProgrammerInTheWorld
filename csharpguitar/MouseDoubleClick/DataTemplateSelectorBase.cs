4f13dbf9f9644d20d1785924a3d7ee4d113a1582
Benjamin Perkins <benperk@microsoft.com>
2020-06-10 09:43:31 +0200

    Add files via upload

---
+++ csharpguitar/MouseDoubleClick/DataTemplateSelectorBase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Media;
+
+namespace FilterWPF
+{
+    public class DataTemplateSelectorBase : DataTemplateSelector
+    {
+        protected MainWindow GetMainWindow(DependencyObject inContainer)
+        {
+            DependencyObject c = inContainer;
+            while (true)
+            {
+                DependencyObject p = VisualTreeHelper.GetParent(c);
+
+                if (c is MainWindow)
+                {
+                    return c as MainWindow;
+                }
+                else
+                {
+                    c = p;
+                }
+            }
+        }
+
+        public override DataTemplate SelectTemplate(object inItem, DependencyObject inContainer)
+        {
+            DataRowView row = inItem as DataRowView;
+
+            if (row != null)
+            {
+                if (row.DataView.Table.Columns.Contains("Status"))
+                {
+                    MainWindow w = GetMainWindow(inContainer);
+                    return (DataTemplate)w.FindResource("StatusImage");
+                }
+            }
+            return null;
+        }
+
+    }
+}
