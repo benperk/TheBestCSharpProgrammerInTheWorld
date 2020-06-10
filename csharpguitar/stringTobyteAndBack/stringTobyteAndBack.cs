ee160fc4fe0bde89139d18a8ec2e3701dfbd11c4
Benjamin Perkins <benperk@microsoft.com>
2020-06-10 09:37:38 +0200

    Add files via upload

---
+++ csharpguitar/stringTobyteAndBack/stringTobyteAndBack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace stringTobyteAndBack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aString = "The Best C# Programmer in the World-Ben Perkins";
+            ASCIIEncoding ascii = new ASCIIEncoding();
+            byte[] byteArray = ascii.GetBytes(aString);
+
+            Console.WriteLine("Converted '" + aString + "' to a byte[].");
+
+            Encoding encoder = new ASCIIEncoding();
+            string bString = encoder.GetString(byteArray);
+
+            Console.WriteLine("Converted the byte[] back to '" + bString + "'.");
+
+            Console.ReadLine();
+        }
+    }
+}
